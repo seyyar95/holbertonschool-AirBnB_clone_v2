ef4f14f96cdc004a24520c3390039a3421036b51
valibm <vmustafayev2001@gmail.com>
2024-03-31 14:29:31 +0000

    Updated reload func

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,6 +37,30 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
+
     def delete(self, obj=None):
         """
         Deletes obj from __objects if it's inside
