2f7c8b40e7eb670dea87b67a3d7b83c805d394a7
valibm <vmustafayev2001@gmail.com>
2024-03-31 14:14:55 +0000

    Added all func

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,3 +7,13 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if value.__class__.__name__ == cls.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
