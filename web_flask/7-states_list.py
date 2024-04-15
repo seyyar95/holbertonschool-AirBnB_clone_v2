4890f3acb3885e726384c6fbb09b42b895bee179
Sayyar Haydarov <heydrovsyyar@gmail.com>
2024-04-15 11:56:24 +0400

    Listing state objects sorted by name (A->Z)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application"""
+
+
 from flask import Flask, render_template
 from models import storage
 
@@ -7,6 +11,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Lists all State objects sorted by name"""
     obj = list(storage.all("State").values())
     states = sorted(obj, key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
@@ -14,6 +19,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """Removes the current SQLAlchemy session"""
     storage.close()
 
 
