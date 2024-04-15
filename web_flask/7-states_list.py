82a1d53a7e5ace5cf6932d869f5432695ece75da
seyyar95 <heydrovsyyar@gmail.com>
2024-04-15 09:15:03 +0000

    List of all State objects sorted by name (A->Z)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Lists all State objects sorted by name"""
-    obj = storage.all("State").values()
+    obj = list(storage.all(State).values())
     states = sorted(obj, key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
