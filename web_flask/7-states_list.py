df18235b9464eaccf74683660770dac71683b2c3
seyyar95 <heydrovsyyar@gmail.com>
2024-04-15 10:34:40 +0000

    list constructor removed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Lists all State objects sorted by name"""
-    obj = list(storage.all(State).values())
+    obj = storage.all(State).values()
     states = sorted(obj, key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
